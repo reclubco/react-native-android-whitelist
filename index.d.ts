d2e10e29e30e728225a25485cf08c4055c94cd0a
Hoang Nguyen <kaisv7n@gmail.com>
2018-12-12 18:30:31 +0700

    Update return void

--- index.d.ts
+++ index.d.ts
@@ -4,11 +4,7 @@ import { NativeModules, Platform } from 'react-native'
 const { RNAndroidWhitelist } = NativeModules
 
 class AndroidWhitelist {
-  static alert = (config: any) => {
-    if (Platform.OS === 'android') {
-      RNAndroidWhitelist.AlertIfWhitelist(config.title, config.text, config.doNotShowAgainText, config.positiveText, config.negativeText)
-    }
-  }
+  static alert(config: any): void
 }
 
 export default AndroidWhitelist
